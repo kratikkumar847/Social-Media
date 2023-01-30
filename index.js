0a96d3c74b242d53fc7a558c933fcb336b533958
Kratik Kumar <kratikkumar847@gmail.com>
2023-01-30 11:31:39 +0530

    vercel-config-4

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
+app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
